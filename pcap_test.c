8549e6447b92d4083d6e75ee7636223f4ff886fe
AlchemicSH <dltpgks01@naver.com>
2017-07-18 05:10:25 +0900

    Modify packet information print about Ethernet and IPv4

--- pcap_test.c
+++ pcap_test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pcap.h>
 
 int main()
@@ -54,18 +55,19 @@ int main()
 
 	while((result = pcap_next_ex(handle, &header, &packet)) >= 0)
 	{
-		if(result == 0) continue;
+		if(result == 0) continue;  // There is no packet.
 
-		if(result == -1) break;
+		if(result == -1) break;    // Signal Lost.
 
-		printf("-------------------------------------------------\n\n");
+		// Print packet.
+		printf("-----------------------------------------------------------\n");
 		printf("Jacked a packet with length of [%d]\n", header->len);
-		printf("-------------------------------------------------\n\n");
+		printf("-----------------------------------------------------------\n");
 
 		printf(" %02x ", packet[0]);
 		enter_cnt = 1;
 
-		for(i = 0 ; i < header->len ; ++i)
+		for(i = 1 ; i < header->len ; ++i)
 		{
 			printf(" %02x ", packet[i]);
 			++enter_cnt;
@@ -75,8 +77,53 @@ int main()
 				printf("\n");
 			}
 		}
-		printf("\n-----------------------------------------------\n");
-		printf("\n\n\n");
+		printf("\n-----------------------------------------------------------\n");
+
+		// Analysis packet information
+		// First, ethernet information
+		printf("[Ethernet]\n");
+		printf("Source Mac Address: ");
+		printf("%02x", packet[6]);
+		for(i = 7 ; i < 12 ; ++i)
+		{
+			printf(":%02x", packet[i]);
+		}
+		printf("\n");
+
+		printf("Destination Mac Address: ");
+		printf("%02x", packet[0]);
+		for(i = 1 ; i < 6 ; ++i)
+		{
+			printf(":%02x", packet[i]);
+		}
+		printf("\n\n");
+
+		// Second, IPv4 information
+		if(packet[12] == 0x08 && packet[13] == 0x00)
+		{
+			printf("This packet type is IPv4!\n");
+			printf("[IPv4]\n");
+			printf("Source IP Address: %d", packet[14 + 12]);
+			for(i = 1 ; i < 4 ; ++i)
+			{
+				printf(".%d", packet[14 + 12 + i]);
+			}
+			printf("\n");
+
+			printf("Destination IP Address: %d", packet[14 + 16]);
+			for(i = 1 ; i < 4 ; ++i)
+			{
+				printf(".%d", packet[14 + 16 + i]);
+			}
+			printf("\n");
+		}
+		else
+		{
+			printf("This packet type is not IPv4!\n");
+			continue;
+		}
+
+		printf("\n\n");
 	}
 	
 	pcap_close(handle);
